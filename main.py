356930f0ddb2d762058007b553e91866ce9a7cc9
zer0silver <duddms0_0@naver.com>
2023-11-16 00:00:41 +0900

    feat: pytorch 모델 로드 후 문장 유사도 계산

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
+from service.grade import *
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+"""
+request 모범 답안, 학생 답안
+response 채점 결과
+"""
+@app.get("/grade")
+async def get_grade(request: GetGradeRequest):
+    return predict_grade(request)
