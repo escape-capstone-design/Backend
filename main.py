ed9d804c1fc62257539c47f0cc1668c270d66b33
zer0silver <duddms0_0@naver.com>
2023-11-21 00:30:23 +0900

    fix: model load with cpu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
 
 from service.feedback import get_feedback
 from service.grade import *
@@ -6,26 +6,13 @@ from service.grade import *
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-#feedback 요청
+"""
+request 질문, 모범 답안, 학생 답안
+response 피드
+"""
 @app.post("/feedback")
 async def feedback(data: dict):
     try:
-        #임시
-        #question = "태양 에너지와 광합성이 지구의 기후 및 환경에 어떤 영향을 미치는지에 대해 설명하십시오."
-        #answer = "태양으로부터 오는 에너지는 식물들에 의해 광합성에 사용되며, 이것은 지면의 열을 증가시키고 대기온도와 함께 바람 및 물 변화량과 연결된다."
-        #tudent_answer = "태양 에너지는 지구의 기온을 낮추고 대기의 기온을 하락시키며, 식물의 광합성에 영향을 주지 않습니다."
-        
-        #실제 front에서 넘어오는 question, answer, student_answer
         question = data.get("question")
         answer = data.get("answer")
         student_answer = data.get("student_answer")
@@ -41,6 +28,6 @@ async def feedback(data: dict):
 request 모범 답안, 학생 답안
 response 채점 결과
 """
-@app.get("/grade")
+@app.post("/grade")
 async def get_grade(request: GetGradeRequest):
     return predict_grade(request)
--- service/grade.py
+++ service/grade.py
@@ -6,12 +6,12 @@ from schema.grade_schema import GetGradeRequest
 
 def predict_grade(request: GetGradeRequest):
     # 파인 튜닝 모델 불러오기
-    model = torch.load('./model/model.pt')
+    device = torch.device('cpu')
+    model = torch.load('./model/model.pt', map_location=device)
     model.eval()
 
     # 두 문장을 모델로 임베딩
-    embeddings = model.encode([request.correct_answer, request.answer], convert_to_tensor=True)
+    embeddings = model.encode([request.correct_answer, request.answer], convert_to_tensor=True, device='cpu')
 
     # 코사인 유사도 계산
-    cosine_score = util.pytorch_cos_sim(embeddings[0], embeddings[1])
-    return cosine_score
+    return util.pytorch_cos_sim(embeddings[0], embeddings[1])[0][0].item()
