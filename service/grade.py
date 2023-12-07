3a140016949bf0933ceca47ede064134f340d5c5
tnals2384 <tnals2384@gmail.com>
2023-12-07 16:49:42 +0900

    fix: state_dict로 모델 로드

--- service/grade.py
+++ service/grade.py
@@ -1,7 +1,7 @@
 import torch
 from enum import Enum
 
-from sentence_transformers import util
+from sentence_transformers import util,models,SentenceTransformer
 from schema.grade_schema import PredictGradeRequest, PredictGradeResponse
 
 
@@ -14,8 +14,25 @@ class TestResult(Enum):
 
 def predict_grade(request: PredictGradeRequest):
     # 파인 튜닝 모델 불러오기
+    # Load Embedding Model
+    embedding_model = models.Transformer(
+        model_name_or_path="BM-K/KoSimCSE-roberta-multitask",
+        max_seq_length=150,
+        do_lower_case=True
+    )
+
+    # Only use Mean Pooling -> Pooling all token embedding vectors of sentence.
+    pooling_model = models.Pooling(
+        embedding_model.get_word_embedding_dimension(),
+        pooling_mode_mean_tokens=True,
+        pooling_mode_cls_token=False,
+        pooling_mode_max_tokens=False,
+    )
+
+    model = SentenceTransformer(modules=[embedding_model, pooling_model])
     device = torch.device('cpu')
-    model = torch.load('./model/model.pt', map_location=device)
+    #model = torch.load('./model/model.pt', map_location=device)
+    model.load_state_dict(torch.load('./model/model_state_dict.pt',map_location=device),strict=False)
     model.eval()
 
     # 두 문장을 모델로 임베딩
